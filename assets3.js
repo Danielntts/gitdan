6a1f2b807e690ecf82b8b0029648f8d30b403db7
Danielntts <147212214+Danielntts@users.noreply.github.com>
2023-10-21 20:00:50 -0300

    feat: created home page

---
+++ assets3.js
@@ -0,0 +1,16 @@
+var button = document.getElementById('read_button');
+
+
+button.addEventListener('click', function() {
+
+  var card = document.querySelector('.card');
+
+  card.classList.toggle('active');
+
+  if (card.classList.contains('active')) {
+
+    return button.textContent = 'Ler menos';
+  }
+
+  button.textContent = 'Read more';
+});
